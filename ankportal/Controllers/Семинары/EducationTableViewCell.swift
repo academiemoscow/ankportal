af4cb4aec51d911983b995df0ee814d5d7d49db9
Oleg Rachkov <oleg.rachkov@gmailcom>
2019-03-29 17:57:52 +0300

    -jhgfjhgj

---
+++ ankportal/Controllers/Семинары/EducationTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  SeminarsTableViewCell.swift
+//  ankportal
+//
+//  Created by Олег Рачков on 26/03/2019.
+//  Copyright © 2019 Academy of Scientific Beuty. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SeminarsTableViewCell: UITableViewCell {
+    
+    let layout = UICollectionViewFlowLayout()
+    var mainPageController: UIViewController?
+    
+    lazy var seminarsCollectionView: EducationListCollectionView = {
+        let seminars = EducationListCollectionView(frame: bounds, collectionViewLayout: layout)
+        seminars.translatesAutoresizingMaskIntoConstraints = false
+        return seminars
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.addSubview(seminarsCollectionView)
+        seminarsCollectionView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        seminarsCollectionView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+        seminarsCollectionView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        seminarsCollectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
