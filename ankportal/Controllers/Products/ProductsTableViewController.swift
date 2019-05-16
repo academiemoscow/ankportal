571aed5276157aa2edb5a48a5291bf994c7f95d3
Timofey Podolskiy <podolskiit@academie.ru>
2019-05-16 18:32:39 +0300

    -

--- ankportal/Controllers/Products/ProductsTableViewController.swift
+++ ankportal/Controllers/Products/ProductsTableViewController.swift
@@ -13,7 +13,12 @@ class ProductsTableViewController: UITableViewController {
     private let defaultCellId = "defaultProductCell"
     private let placeholderCellId = "placeholderProductCell"
     private let navigationBarColor = UIColor(r: 159, g: 131, b: 174)
+    
     private let ankportalREST = ANKRESTService(type: .productList)
+    var restParametres: [RESTParameter] = [
+        RESTParameter(filter: .pageSize, value: "5"),
+        RESTParameter(filter: .pageNumber, value: "1")
+    ]
     
     var data = [ProductPreview]()
     var isLoading = true
@@ -60,11 +65,7 @@ class ProductsTableViewController: UITableViewController {
     }
     
     func fetchData() {
-        ankportalREST.add(parameters: [
-            RESTParameter(filter: .pageSize, value: "5"),
-            RESTParameter(filter: .pageNumber, value: "1")
-        ])
-        ankportalREST.execute { [weak self] (data, respone, error) in
+        ankportalREST.execute(withParametres: restParametres) { [weak self] (data, respone, error) in
             if ( error != nil ) {
                 print(error!)
                 return
@@ -112,50 +113,5 @@ class ProductsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 400
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
