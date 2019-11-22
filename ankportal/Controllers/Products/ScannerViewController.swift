c386ec2c817af031c66eea018a37d20536b9a4d7
Oleg <oleg.rachkov@gmail.com>
2019-11-22 19:44:51 +0300

    h

--- ankportal/Controllers/Products/ScannerViewController.swift
+++ ankportal/Controllers/Products/ScannerViewController.swift
@@ -161,24 +161,41 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
         
         var startIndex = code.index(code.startIndex, offsetBy: 6)
         var endIndex = code.index(code.startIndex, offsetBy: 11)
-        var article = code[startIndex...endIndex]
         
-        articleArray.append(article.description+"0")
-    
-        startIndex = code.index(code.startIndex, offsetBy: 6)
-        endIndex = code.index(code.startIndex, offsetBy: 10)
-        article = code[startIndex...endIndex]
-        articleArray.append(article.description+"00")
-
-        startIndex = code.index(code.startIndex, offsetBy: 7)
-        endIndex = code.index(code.startIndex, offsetBy: 11)
-        article = code[startIndex...endIndex]
-        articleArray.append(article.description+"0")
+        for i in 5...9 {
+            for j in 3...5 {
+                if i+j < 13 {
+                    startIndex = code.index(code.startIndex, offsetBy: i)
+                    endIndex = code.index(code.startIndex, offsetBy: i+j)
+                    let article = code[startIndex...endIndex]
+                    articleArray.append(article.description)
+                    articleArray.append(article.description+"0")
+                    articleArray.append(article.description+"00")
+                }
+            }
+            
+        }
         
-        startIndex = code.index(code.startIndex, offsetBy: 9)
-        endIndex = code.index(code.startIndex, offsetBy: 12)
-        article = code[startIndex...endIndex]
-        articleArray.append(article.description)
+//        var startIndex = code.index(code.startIndex, offsetBy: 6)
+//        var endIndex = code.index(code.startIndex, offsetBy: 11)
+//        var article = code[startIndex...endIndex]
+//
+//        articleArray.append(article.description+"0")
+//
+//        startIndex = code.index(code.startIndex, offsetBy: 6)
+//        endIndex = code.index(code.startIndex, offsetBy: 10)
+//        article = code[startIndex...endIndex]
+//        articleArray.append(article.description+"00")
+//
+//        startIndex = code.index(code.startIndex, offsetBy: 7)
+//        endIndex = code.index(code.startIndex, offsetBy: 11)
+//        article = code[startIndex...endIndex]
+//        articleArray.append(article.description+"0")
+//
+//        let startIndex = code.index(code.startIndex, offsetBy: 9)
+//        let endIndex = code.index(code.startIndex, offsetBy: 12)
+//        let article = code[startIndex...endIndex]
+//        articleArray.append(article.description)
 
         ankREST.add(parameters: (articleArray.mapToRESTParameters(forRESTFilter: .article)))
         
@@ -195,10 +212,6 @@ class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDel
                 self?.showProductView(product: data[0])
             }
         }
-        
-
-        
-        
         
     }
     
