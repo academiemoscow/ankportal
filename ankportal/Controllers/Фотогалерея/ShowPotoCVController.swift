b98697b4ea4efaec5fa3d33cb4c7a17d9dcf069d
Oleg <oleg.rachkov@gmail.com>
2020-01-28 16:53:04 +0300

    111

---
+++ ankportal/Controllers/Фотогалерея/ShowPotoCVController.swift
@@ -0,0 +1,9 @@
+//
+//  ShowPotoCVController.swift
+//  ankportal
+//
+//  Created by Олег on 28/01/2020.
+//  Copyright © 2020 Academy of Scientific Beuty. All rights reserved.
+//
+
+import Foundation
